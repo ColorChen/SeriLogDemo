21e53687386104ba391d0b9c33d2b09acdce90f8
DESKTOP-C88JCN7\User <tidauna@gmail.com>
2022-11-03 15:05:49 +0800

    SeriLog修改appSetting檔案、Program.cs Middleware 設定方式

--- SeriLogDemo_Net6/Program.cs
+++ SeriLogDemo_Net6/Program.cs
@@ -1,23 +1,61 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+   .ReadFrom.Configuration(builder.Configuration)
+   .CreateLogger();
+
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        // 如果要自訂訊息的範本格式，可以修改這裡，但修改後並不會影響結構化記錄的屬性
+        // options.MessageTemplate = "Handled {RequestPath}";
+
+        // 預設輸出的紀錄等級為 Information，你可以在此修改記錄等級
+        // options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
+
+        // 你可以從 httpContext 取得 HttpContext 下所有可以取得的資訊！
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            diagnosticContext.Set("UserID", httpContext.User.Identity?.Name);
+        };
+    });
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch(Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseAuthorization();
 
-app.MapControllers();
-
-app.Run();
