3121a04f7eb8855608668290db65e375b85ca1a8
DESKTOP-C88JCN7\User <tidauna@gmail.com>
2022-11-04 14:29:09 +0800

    擴充IApplicationBuilder，包裝SeriLogRequstResponseLogging 到CustomMiddleware靜態類別

---
+++ SeriLogDemo_Net6/Extensions/CustomMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+
+namespace SeriLogDemo_Net6.Extensions
+{
+    public static class CustomMiddleware
+    {
+        public static IApplicationBuilder UseSerilogRequestResponseLogging(this IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+            builder.UseSerilogRequestLogging(opts => {
+                opts.EnrichDiagnosticContext = LogHelper.EnrichFromRequest;
+            });
+            return builder;
+        }
+    }
+}
--- SeriLogDemo_Net6/Program.cs
+++ SeriLogDemo_Net6/Program.cs
@@ -2,6 +2,7 @@
 using Serilog.Events;
 using Serilog.Extensions.Hosting;
 using SeriLogDemo_Net6;
+using SeriLogDemo_Net6.Extensions;
 using SeriLogDemo_Net6.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -40,46 +41,13 @@ try
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-
-    #region UseSerilogRequestLogging 參考範例
-    //app.UseSerilogRequestLogging(options =>
-    //{
-    //    // 如果要自訂訊息的範本格式，可以修改這裡，但修改後並不會影響結構化記錄的屬性
-    //    // options.MessageTemplate = "Handled {RequestPath}";
-
-    //    // 預設輸出的紀錄等級為 Information，你可以在此修改記錄等級
-    //    // options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
-
-    //    // 你可以從 httpContext 取得 HttpContext 下所有可以取得的資訊！
-    //    options.EnrichDiagnosticContext = async (diagnosticContext, httpContext) =>
-    //    {
-    //        diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
-    //        diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
-
-    //        #region 自定義的屬性
-
-
-    //        diagnosticContext.Set("RequestQueryString", httpContext.Request.QueryString.Value);
-    //        diagnosticContext.Set("Protocol", httpContext.Request.Protocol);
-    //        diagnosticContext.Set("RequestBody", await LogHelper.ReadBodyFromRequest(httpContext.Request));
-
-    //        // Set the content-type of the Response at this point
-    //        diagnosticContext.Set("ContentType", httpContext.Response.ContentType);
-    //        #endregion
-
-    //        diagnosticContext.Set("UserID", httpContext.User.Identity?.Name);
-    //    };
-    //});
-    #endregion
-    app.UseMiddleware<RequestResponseLoggingMiddleware>();
-    app.UseSerilogRequestLogging(opts => {
-        opts.EnrichDiagnosticContext = LogHelper.EnrichFromRequest;
-    });
-
+    app.UseSerilogRequestResponseLogging();
 
     app.UseAuthorization();
 
     app.MapControllers();
+
+    
 
     app.Run();
 }
